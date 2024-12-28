435ad2b201f476ba658ac410ec2d6e9e4e87afc3
nomankarim8 <karimnoman97@gmail.com>
2024-12-28 04:59:53 -0500

    commit

---
+++ SquarePattern.java
@@ -0,0 +1,12 @@
+public class SquarePattern {
+    public static void main(String[] args) {
+        int size = 5; // Size of the square
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
+            }
+            
+            System.out.println();
+        }
+    }
+}
