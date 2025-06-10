f5ca0dac98430436a92793d8e10b52775c3320a1
nomankarim8 <karimnoman97@gmail.com>
2025-06-10 20:02:33 +0600

    update 49 files

--- mail/contact.js
+++ mail/contact.js
@@ -3,7 +3,7 @@ $(function () {
     $("#contactForm input, #contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function ($form, event, errors) {
-        },
+        }, 
         submitSuccess: function ($form, event) {
             event.preventDefault();
             var name = $("input#name").val();
--- mail/contact.php
+++ mail/contact.php
@@ -3,7 +3,7 @@ if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message'])
   http_response_code(500);
   exit();
 }
-
+ 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
